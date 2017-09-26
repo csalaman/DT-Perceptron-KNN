edd81d4e4c69b2619c6da9e7f2d2bde497ce7861
Carlos Salamanca <csalaman@terpmail.umd.edu>
2017-09-26 01:06:45 -0400

    Add files via upload
    Finished DT

--- testDecisionTree.py
+++ testDecisionTree.py
@@ -5,5 +5,8 @@ import numpy
 import dt
 
 # Test the ZDecision Tree
-curve = run.learningCurveSet(dt.DT({'maxDepth':5}),data.SentimentData)
-run.plotCurve("DT Learning Curve on DAta", curve)
+curve = run.learningCurveSet(dt.DT({'maxDepth':6}),data.SentimentData)
+run.plotCurve('Decision Tree Learning Curve on Sediment Data',curve)
+
+curve = run.hyperparamCurveSet(dt.DT({}), 'maxDepth', [1,2,4,6,8,12,16],data.SentimentData)
+run.plotCurve('Decision Tree Hyperparameter Curve on Sediment Data',curve)
